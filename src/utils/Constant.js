c45990beee3bbcb1df317fb9746f7dccaa6c5032
ahmar7 <ahmarjabbar7@gmail.com>
2024-04-17 00:06:06 +0500

    new

--- src/utils/Constant.js
+++ src/utils/Constant.js
@@ -3,6 +3,6 @@ module.exports = {
   // baseUrl: "http://localhost:4000/api/v1",
   // Local server
   // Official server
-  baseUrl: "https://blockchain-servertx.onrender.com/api/v1",
+  baseUrl: "https://defiblock-server-u052.onrender.com/api/v1",
   // Official server
 };
